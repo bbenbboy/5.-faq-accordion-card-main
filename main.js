48d8558cd5199eed88cdd9a5e21b642480aa98cf
Ratchapon Ngamdan <ratchapon.ngam@gmail.com>
2023-01-27 14:38:29 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,14 @@
+"user strict";
+
+const hidden = document.querySelector(".hidden");
+const active = document.querySelector(".active");
+const hover = document.querySelector(".hover");
+const rotateArrow = document.querySelector(".rotate-arrow");
+
+const faqHover = document.querySelectorAll("faqHover dt");
+const contents = document.querySelectorAll(".contents dd");
+const arrowDown = document.querySelectorAll(".arrow-down");
+
+function click() {
+    faqHover.
+}
